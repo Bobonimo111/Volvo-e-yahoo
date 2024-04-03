8c3ad7b55c6e721585f9e16f2e1fa95334346d63
William Rodrigues <carlosmaycon443@gmail.com>
2024-04-03 14:40:16 -0300

    inital commit

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+    const volvo = document.querySelectorAll(".container-main>section")[0]
+    const yah = document.querySelectorAll(".container-main>section")[1]
+
+    console.log(volvo)
+})
